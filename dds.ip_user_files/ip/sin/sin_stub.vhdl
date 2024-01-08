d2396ca82ddf5db9a0d76e12b3874936b9f4d4f8
黛亭 <75159821+purplefrigid@users.noreply.github.com>
2024-01-08 20:31:47 +0800

    Add files via upload

---
+++ dds.ip_user_files/ip/sin/sin_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+-- Date        : Tue May 16 12:11:23 2023
+-- Host        : DESKTOP-5J050JL running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/Users/zgnhz/Desktop/graduatedesign/dds/dds.srcs/sources_1/ip/sin/sin_stub.vhdl
+-- Design      : sin
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity sin is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 8 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end sin;
+
+architecture stub of sin is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[8:0],douta[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_1,Vivado 2018.1";
+begin
+end;
---
+++ dds.ip_user_files/ip/squ/squ_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+// Date        : Sat May 13 23:27:56 2023
+// Host        : DESKTOP-5J050JL running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top squ -prefix
+//               squ_ squ_stub.v
+// Design      : squ
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_1,Vivado 2018.1" *)
+module squ(clka, addra, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,addra[8:0],douta[7:0]" */;
+  input clka;
+  input [8:0]addra;
+  output [7:0]douta;
+endmodule
